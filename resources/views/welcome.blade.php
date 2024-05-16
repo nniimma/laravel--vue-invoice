dedfa4c880df45505a848e593dbab408d5e738f4
Nima <alireza.khazforoosh@adventistas.org>
2024-05-16 14:25:30 -0300

    update some styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Invoice App</title>
-    <link rel="icon" href="{{ URL::asset('images/invoice.png') }}">
+    <link rel="icon" href="{{ URL::asset('images/icon.png') }}">
     {{-- @vite('resources/css/app.css') --}}
 </head>
 
